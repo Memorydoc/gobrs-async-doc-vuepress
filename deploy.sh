bac08dd89efb17d33d82b8c82b6dcb87ea8003a4
sizegang1 <sizegang1@jd.com>
2022-11-27 19:17:09 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,20 @@ npm run build
 
 # 执行完该文件后 手动到 dist 下的index.html 中 将  /assets/ 全部替换 为以下地址
 # https://cdn.jsdelivr.net/gh/Memorydoc/gobrs-async.github.io/assets/
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+echo 'async.sizegang.cn' > CNAME
+
+git init
+git add -A
+git commit -m "auto commit"
+
+# github
+git branch -m master gh-pages
+git remote add origin git@github.com:Memorydoc/gobrs-async.github.io.git
+git push -u origin gh-pages -f
+
+cd -
+rm -rf docs/.vuepress/dist
